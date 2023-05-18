d533112487d5ff8a22d2d6d7c882b3c73ad1936a
phuc <phamhuulequocphuc@gmail.com>
2023-05-18 22:59:47 +0700

    solidity claim

--- dapp/Claim.vue
+++ dapp/Claim.vue
@@ -2,12 +2,16 @@
 import {ref, onMounted, computed} from 'vue'
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
-import { configureChains, createConfig } from '@wagmi/core'
-import { goerli } from '@wagmi/core/chains'
-import { getAccount, readContract } from '@wagmi/core'
+import { ConnectorAlreadyConnectedError, configureChains, createConfig } from '@wagmi/core'
+import { optimismGoerli } from '@wagmi/core/chains'
+import { getAccount, writeContract } from '@wagmi/core'
+import { ethers } from 'ethers'
+import { ChainDoesNotSupportContract } from 'viem'
 
-const chains = [goerli]
+const chains = [optimismGoerli]
 const projectId = '10ee9683173a9bdd4179a59949c384c7'
+const claimContractAddress = '0x71f67046625806ca5f722220D38adb8C78534574'
+const verifierPubkey = '0x872219408d852490d8a04332ab8b50b62abb22599bcce177fc94b624e9a587381644'
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
@@ -17,8 +21,21 @@ const wagmiConfig = createConfig({
 })
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const web3modal = new Web3Modal({ projectId }, ethereumClient)
-
+const p = (a: string) => ethers.solidityPackedKeccak256(["address"],[a])
 const claim = async () => {
+    const a = getAccount()
+    const s = new ethers.Wallet(verifierPubkey.substring(6))
+    const n = await s.signMessage(ethers.toBeArray(p(a.address || '')))
+    const { hash: h } = await writeContract({
+        address: claimContractAddress,
+        abi: [{ "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "nonce", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "address", "name": "referrer", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "timestamp", "type": "uint256" }], "name": "Claim", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [], "name": "GENERAL", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "REFFERALS", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "_claimedUser", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "_usedNonce", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "nonce", "type": "uint256" }, { "internalType": "bytes", "name": "signature", "type": "bytes" }, { "internalType": "address", "name": "referrer", "type": "address" }], "name": "claim", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "claimedCount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimedPercentage", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimedSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "disableClaiming", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "enableClaiming", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "inviteRewards", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "inviteUsers", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "isClaimingEnabled", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "path", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "recover", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "referReward", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_newroute", "type": "uint256" }], "name": "routeUpdate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_tokenAddress", "type": "address" }], "name": "setToken", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "val", "type": "address" }], "name": "setVerifier", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_newgen", "type": "uint256" }], "name": "setgeneral", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "token", "outputs": [{ "internalType": "contract IERC20", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "transferBal", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_token", "type": "address" }], "name": "transferTokens", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "verifier", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }],
+        functionName: 'claim',
+        args: [ethers.toBigInt(Math.floor(Math.random()*10000)), `0x${n.substring(2)}`, `0x${s.address.substring(2)}`],
+        value: ethers.toBigInt(0)
+    })
+
+    // @ts-ignore
+    alert(`Claimed ${h}`)
 }
 </script>
 
--- dapp/claim.md
+++ dapp/claim.md
@@ -7,13 +7,9 @@ Read the [help section](#help) for more information
 <Claim/>
 
 ## Help
-1. Click Connect button to connect this dapp with Phantom wallet
-2. Enter amount that you want to swap
-3. Click on the "SOL ==> MOVE" or "MOVE ==> SOL"
-4. Approve the transaction on Phantom wallet
-5. Wait for the transaction to complete
+1. Click Connect button to connect this dapp with wallet
+2. Click Claim and Approve button
 
-You can also see the transaction id, balances, and its link on Solana Explorer
 <script setup language="ts">
 import Claim from './Claim.vue'
 </script>
